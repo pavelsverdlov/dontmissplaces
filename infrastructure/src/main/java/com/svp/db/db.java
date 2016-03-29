dc68805b4f1d451e2a307554f064336ec34c19c1
pavelsverdlov <sverdlov24@gmail.com>
2016-03-29 23:18:37 +0300

    add db, open gps

---
+++ infrastructure/src/main/java/com/svp/db/db.java
@@ -0,0 +1,6 @@
+package mvpvs.infrastructure.com.infrastructure.db;
+
+import android.util.Log;
+
+public class db {
+}
---
+++ infrastructure/src/main/java/com/svp/db/ormlite/DatabaseOrmLiteHelper.java
@@ -0,0 +1,43 @@
+package com.svp.db.ormlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * Created by Pasha on 3/22/2016.
+ */
+public abstract class DatabaseOrmLiteHelper extends OrmLiteSqliteOpenHelper {
+
+    private final String className;
+    public DatabaseOrmLiteHelper(Context context, String databaseName, int databaseVersion) {
+        super(context, databaseName, null, databaseVersion);
+        className = DatabaseOrmLiteHelper.class.getName();
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+        Log.i(className, "onCreate");
+        onTableCreate(db,connectionSource);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        Log.i(className, "onUpgrade");
+        onTableUpdate(db, connectionSource);
+    }
+    @Override
+    public void close() {
+        super.close();
+//        simpleDao = null;
+//        simpleRuntimeDao = null;
+    }
+
+    protected abstract void onTableCreate(SQLiteDatabase db,ConnectionSource connectionSource);
+    protected abstract void onTableUpdate(SQLiteDatabase db,ConnectionSource connectionSource);
+
+}
