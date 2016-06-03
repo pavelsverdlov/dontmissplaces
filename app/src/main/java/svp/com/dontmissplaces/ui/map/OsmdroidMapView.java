a8886bc4b60e4dd72a40904fb139250652e32f50
pavel.sverdlov <pavel.sverdlov@techinsign.ua>
2016-06-03 11:23:42 +0300

    add my location

--- app/src/main/java/svp/com/dontmissplaces/ui/map/OsmdroidMapView.java
+++ app/src/main/java/svp/com/dontmissplaces/ui/map/OsmdroidMapView.java
@@ -8,18 +8,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 
 import org.osmdroid.api.IMapController;
-import org.osmdroid.bonuspack.location.NominatimPOIProvider;
-import org.osmdroid.bonuspack.location.POI;
+import org.osmdroid.bonuspack.location.*;
 import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.util.TileSystem;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.FolderOverlay;
-import org.osmdroid.views.overlay.MapEventsOverlay;
-import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.Overlay;
-import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.*;
+import org.osmdroid.views.overlay.compass.*;
+import org.osmdroid.views.overlay.mylocation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -33,41 +30,47 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
     private final ActivityPermissions permissions;
     private final MapView mapView;
     private final IMapController mapController;
-    FolderOverlay poiMarkers;
-    //   private MyLocationNewOverlay locationOverlay;
-    //   private final ArrayList<OverlayItem> overlayItemArray;
+    private final MyLocationNewOverlay myLocationOverlay;
+    private final GpsMyLocationProvider gpsMyLocationProvider;
+    private final FolderOverlay poiMarkers;
+    private final CompassOverlay compassOverlay;
 
     public OsmdroidMapView(Activity activity, ActivityPermissions permissions) {
         this.activity = activity;
         this.permissions = permissions;
 
-        TileSystem.setTileSize(2000);
-
-        // overlayItemArray = new ArrayList<OverlayItem>();
         mapView = (MapView) activity.findViewById(R.id.osmdroid_map);
-        mapView.setTileSource(TileSourceFactory.MAPNIK);
-        //set auto resize text titles on map
-        mapView.setTilesScaledToDpi(true);
+        setMapViewSettings();
+
+        compassOverlay = new CompassOverlay(activity, new InternalCompassOrientationProvider(activity),
+                mapView);
+        compassOverlay.enableCompass();
 
         mapController = mapView.getController();
-
-        // DefaultResourceProxyImpl defaultResourceProxyImpl = new DefaultResourceProxyImpl(activity);
-        //MyItemizedIconOverlay myItemizedIconOverlay = new MyItemizedIconOverlay(overlayItemArray, null, defaultResourceProxyImpl);
-
-        //mapView.getOverlays().add(myItemizedIconOverlay);
+        poiMarkers = new FolderOverlay(activity);
 
         MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(activity, this);
-        mapView.getOverlays().add(0, mapEventsOverlay);
 
-        poiMarkers = new FolderOverlay(activity);
+        gpsMyLocationProvider = new GpsMyLocationProvider(activity);
+        myLocationOverlay = new MyLocationNewOverlay(gpsMyLocationProvider, mapView);
+        setMyLocationUpdate(1000);
+
+        mapController.setZoom(12);
+
+        mapView.getOverlays().add(mapEventsOverlay);
+        mapView.getOverlays().add(compassOverlay);
         mapView.getOverlays().add(poiMarkers);
+        mapView.getOverlays().add(myLocationOverlay);
+        mapView.postInvalidate();
     }
 
+    /**
+     * IMapView
+     */
     @Override
     public void showSessionsTrack(Collection<SessionView> sessions) {
 
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         permissions.checkPermissionExternalStorage();
@@ -97,15 +100,10 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
         //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
         */
     }
-
     @Override
     public void onStart() {
-
-        mapController.setZoom(12);
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);//locationOverlay.getMyLocation();//
-
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         mapController.setCenter(startPoint);
-        //  locationOverlay.setDrawAccuracyEnabled(true);
     }
 
     @Override
@@ -154,7 +152,6 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
     }
 
 
-
     @Override
     public boolean singleTapConfirmedHelper(GeoPoint p) {
         drawMarker(p);
@@ -166,8 +163,10 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
     public boolean longPressHelper(GeoPoint p) {
         return false;
     }
+
     Marker startMarker;
-    private void drawMarker(GeoPoint p){
+
+    private void drawMarker(GeoPoint p) {
         if (startMarker != null) {
             startMarker.remove(mapView);
         }
@@ -181,6 +180,7 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
 
         mapView.invalidate();
     }
+
     private void drawLine(GeoPoint s, GeoPoint e) {
         ArrayList<GeoPoint> pp = new ArrayList<GeoPoint>();
         pp.add(s);
@@ -194,21 +194,26 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
         mapView.invalidate();
     }
 
+
+    /**
+     * Points of interest
+     */
     private void getPOIs(GeoPoint startPoint) {
         permissions.checkPermissionNetwork();
         InputData input = new InputData(startPoint, "cinema", 50, 0.1);
         new PointsOfInterestTask().execute(input);
     }
 
-    public final class InputData{
+    public final class InputData {
         public final GeoPoint point;
         public final String keyword;
         public final int maxResults;
         public final double maxDistance;
+
         /**
          * @param maxDistance to the position, in degrees.
-         * Note that it is used to build a bounding box around the position, not a circle.
-         * */
+         *                    Note that it is used to build a bounding box around the position, not a circle.
+         */
         public InputData(GeoPoint point, String keyword, int maxResults, double maxDistance) {
             this.point = point;
             this.keyword = keyword;
@@ -223,7 +228,7 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
             try {
                 InputData data = params[0];
                 NominatimPOIProvider poiProvider = new NominatimPOIProvider(NominatimPOIProvider.NOMINATIM_POI_SERVICE);
-                ArrayList<POI> pois = poiProvider.getPOICloseTo(data.point,data.keyword, data.maxResults, data.maxDistance);
+                ArrayList<POI> pois = poiProvider.getPOICloseTo(data.point, data.keyword, data.maxResults, data.maxDistance);
                 for (Overlay item : poiMarkers.getItems()) {
                     poiMarkers.remove(item);
                 }
@@ -240,7 +245,7 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
 
                     if (poi.mThumbnail != null) {
                         poiMarker.setIcon(new BitmapDrawable(poi.mThumbnail));
-                    }else{
+                    } else {
                         poiMarker.setIcon(poiIcon);
                     }
                     poiMarkers.add(poiMarker);
@@ -256,6 +261,53 @@ public class OsmdroidMapView implements IMapView, MapEventsReceiver {
             }
             return null;
         }
+    }
+
+    /**
+     * My location handlers
+     */
+    private void setMyLocationUpdate(long milliSeconds){
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.setDrawAccuracyEnabled(true);
+
+        gpsMyLocationProvider.startLocationProvider(myLocationOverlay);
+        gpsMyLocationProvider.setLocationUpdateMinDistance(100);
+        gpsMyLocationProvider.setLocationUpdateMinTime(milliSeconds);
+    }
+
+
+    private void setMapViewSettings() {
+        //get value from settings
+        String titleSourceType = "mapnik";
+        OnlineTileSourceBase tileSource;
+        //TODO: refactor to enum
+        switch (titleSourceType) {
+            case "mapnik":
+                tileSource = TileSourceFactory.MAPNIK;
+                break;
+            case "cyclemap":
+                tileSource = TileSourceFactory.CYCLEMAP;
+                break;
+            case "osmpublictransport":
+                tileSource = TileSourceFactory.PUBLIC_TRANSPORT;
+                break;
+            case "mapquestosm":
+                tileSource = TileSourceFactory.MAPQUESTOSM;
+                break;
+            default:
+                tileSource = TileSourceFactory.DEFAULT_TILE_SOURCE;
+                break;
+        }
+
+        mapView.setTileSource(tileSource);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        //set auto resize text titles on map
+        mapView.setTilesScaledToDpi(true);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapView.setMinZoomLevel(3);
+        mapView.setMaxZoomLevel(19); // Latest OSM can go to 21!
+        mapView.getTileProvider().createTileCache();
     }
 
 /*
