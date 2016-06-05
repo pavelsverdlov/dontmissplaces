88d0a91e38fc836604c3e4473ef8a1fcf0c20a2c
pavelsverdlov <sverdlov24@gmail.com>
2016-06-05 23:57:42 +0300

    settings

--- infrastructure/src/main/java/com/svp/infrastructure/PreferenceSettings.java
+++ infrastructure/src/main/java/com/svp/infrastructure/PreferenceSettings.java
@@ -1,6 +1,7 @@
 package com.svp.infrastructure;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
@@ -9,6 +10,20 @@ public class PreferenceSettings {
 
     public PreferenceSettings(Activity activity) {
         this.activity = activity;
+        /*
+        //http://stackoverflow.com/questions/8855069/android-sharedpreferences-best-practices
+        SharedPreferences preferences = activity.getSharedPreferences("com.example.app", Context.MODE_PRIVATE);
+        // get values from Map
+        preferences.getBoolean("key", defaultValue)
+        preferences.get..("key", defaultValue)
+
+        // you can get all Map but be careful you must not modify the collection returned by this
+        // method, or alter any of its contents.
+        Map<String, ?> all = preferences.getAll();
+
+        // get Editor object
+        SharedPreferences.Editor editor = preferences.edit();
+        */
     }
 
     public void putString(String key, String value){
@@ -25,7 +40,7 @@ public class PreferenceSettings {
     }
 
     private SharedPreferences getter(){
-        return PreferenceManager.getDefaultSharedPreferences(activity);
+        return PreferenceManager.getDefaultSharedPreferences(activity,);
     }
     private SharedPreferences.Editor setter() {
         return getter().edit();
