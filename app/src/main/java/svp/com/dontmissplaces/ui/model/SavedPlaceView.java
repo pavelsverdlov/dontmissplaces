9ce24659fc198c9bcd679a00d20231530fe522e7
pavelsverdlov <sverdlov24@gmail.com>
2016-06-19 22:47:27 +0300

    my place class / extend place info

---
+++ app/src/main/java/svp/com/dontmissplaces/ui/model/SavedPlaceView.java
@@ -0,0 +1,10 @@
+package svp.com.dontmissplaces.ui.model;
+
+public class SavedPlaceView {
+    /**
+     * if place has notification then program check distance to this place when
+     * user position changes
+     * */
+    public boolean hasNotification;
+
+}
